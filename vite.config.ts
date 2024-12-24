4dd4c89e93cf081d5b83ae42c3b81f676a32f683
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-12-24 15:39:31 +0000

    changes for deployment settings v5

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [react()],
@@ -8,13 +8,5 @@ export default defineConfig({
 		alias: {
 			'@': path.resolve(__dirname, './src'),
 		},
-	},
-	server: {
-		proxy: {
-			'/use.typekit.net': 'https://use.typekit.net',
-		},
-	},
-	build: {
-		outDir: 'dist',
 	},
 });
